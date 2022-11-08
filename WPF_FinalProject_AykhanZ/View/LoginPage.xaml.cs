a720c27e648c07d788328429abae5bb2a1b8a525
Aykhan <110285172+AyKhanZ@users.noreply.github.com>
2022-11-08 14:19:17 +0400

    Add files via upload

---
+++ WPF_FinalProject_AykhanZ/View/LoginPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using WPF_FinalProject_AykhanZ.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_FinalProject_AykhanZ.View
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Window
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            DataContext = new LoginPage();
+        }
+    }
+}
